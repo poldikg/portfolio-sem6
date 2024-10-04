4c8953176f046ccb404123256fede085a9b14b3f
Ivan <ivan.georgiev2002k@gmail.com>
2024-10-04 14:20:44 +0200

    Adding new images that are being used to represent the project valdle and some descriptive text about the images.

--- src/components/Projects/ProjectData.jsx
+++ src/components/Projects/ProjectData.jsx
@@ -1,63 +1,73 @@
-import Project1 from "./project1.png"; 
-import Project2 from "./project2.png"; 
-import Project3 from "./project3.png"; 
+import Project1 from "./project1.png";
+import Project2 from "./project2.png";
+import Project3 from "./project3.png";
 
- const projectData = [
+//Importing Valdle images
+import Valdlemenu from "../../assets/Valdle/Valdlemenu.png";
+import Valdleagent from "../../assets/Valdle/Valdleagent.png";
+import Valdlemap from "../../assets/Valdle/Valdlemap.png";
+import Valdleskin from "../../assets/Valdle/Valdleskin.png";
+import Valdleability from "../../assets/Valdle/Valdleability.png";
+import Valdlequote from "../../assets/Valdle/Valdlequote.png";
+import Valdleprogression from "../../assets/Valdle/Valdleprogression.png";
+
+
+const projectData = [
+  {
+    title: "VALDLE",
+    image: Valdlemenu,
+    projectDescription: "GAME WHERE YOU GUESS SPECIFIC THINGS ABOUT DIFFERENT TOPICS IN THE GAME VALORANT.",
+    projectData: [{
+      img: Valdlemenu,
+      description: "Main menu of the game where you choose with which challenge to start, after you complete a challenge you can come back after 24 hours to do it again, but the answer is randomized of reach day."
+    },
     {
-      title: "VALDLE",
-      image: Project1,
-      projectDescription: "GAME WHERE YOU GUESS SPECIFIC THINGS ABOUT DIFFERENT TOPICS IN THE GAME VALORANT.",
-      projectData: [{
-        img: Project1,
-        description: "Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 "
-      }, 
-      {
-        img: Project1,
-        description: "Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 "
-      }, 
-      {
-        img: Project1,
-        description: "Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 "
-      },
+      img: Valdleagent,
+      description: "Demonstrating one of the games, here the user can start by choosing a character and get a hint if he is close to guessing the right one by the different sepcification e.g(Role, Species). If the user still has a hard time choosing there are a few more buttons that unlock after a couple of unsuccsessful tries. "
+    },
+    {
+      img: Valdleprogression,
+      description: "Here I am showing a bit of the progression into making the website, I usually start with low-fidelity prototypes which later on I turn into a high-fidelity ones, but since this project was more about me and learning about front-end programming I went into directly high-fidelity prototypes with testing color palettes and fonts."
+    },
     ],
-    sliderImages: [Project1, Project1, Project1, Project1]
+    sliderImages: [Valdlemenu, Valdleagent, Valdlemap, Valdleskin, Valdleability, Valdlequote]
+  },
+  {
+    title: "Bieren's Referral System",
+    image: Project2,
+    projectDescription: "PROJECT DESCRIPTION",
+    projectData: [{
+      img: Project2,
+      description: "Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 "
     },
     {
-      title: "PROJECT 2",
-      image: Project2,
-      projectDescription: "PROJECT DESCRIPTION",
-      projectData: [{
-        img: Project2,
-        description: "Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 "
-      }, 
-      {
-        img: Project2,
-        description: "Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 "
-      }, 
-      {
-        img: Project2,
-        description: "Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 "
-      }],
+      img: Project2,
+      description: "Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 "
+    },
+    {
+      img: Project2,
+      description: "Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 "
+    }],
     sliderImages: [Project2, Project2, Project2, Project2]
+  },
+  {
+    title: "Movies Website(in progress)",
+    image: Project3,
+    projectDescription: "PROJECT DESCRIPTION",
+    projectData: [{
+      img: Project3,
+      description: "Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 "
     },
     {
-      title: "PROJECT 3",
-      image: Project3,
-      projectDescription: "PROJECT DESCRIPTION",
-      projectData: [{
-        img: Project3,
-        description: "Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 Text 1 "
-      }, 
-      {
-        img: Project3,
-        description: "Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 "
-      }, 
-      {
-        img: Project3,
-        description: "Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 "
-      }],
-      sliderImages: [Project3, Project3, Project3, Project3]
-    }, 
-  ];
+      img: Project3,
+      description: "Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 Text 2 "
+    },
+    {
+      img: Project3,
+      description: "Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 Text 3 "
+    }],
+    sliderImages: [Project3, Project3, Project3, Project3]
+  },
+];
 
-  export default projectData
+export default projectData
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, createRef } from "react";
-import "./Projects.css"; 
-import Project1 from "./project1.png"; 
-import Project2 from "./project2.png"; 
-import Project3 from "./project3.png"; 
+import "./Projects.css";
+import Project1 from "./project1.png";
+import Project2 from "./project2.png";
+import Project3 from "./project3.png";
 import { Link, useParams } from "react-router-dom";
 import Project from "../Project/Project";
 import projectData from "./ProjectData";
+
 
 const Projects = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
@@ -13,7 +14,7 @@ const Projects = () => {
   const handleMouseMove = (e) => {
     setMousePosition({ x: e.clientX, y: e.clientY });
   };
-  
+
 
   return (
     <div className="projects-section" onMouseMove={handleMouseMove} id="projects" >
@@ -22,27 +23,27 @@ const Projects = () => {
       <div className="projects">
         {projectData.map((project, index) => (
           <Link to="/Project" state={project}>
-          <div
-            className="project image-hover"
-            key={index}
-            style={{ "--i": index }}
-          >
-            <span className="project-nr">{index + 1}</span>
-            <h2 className="title">
-              {project.title}
-            </h2>
-            
-            <img
-              src={project.image}
-              alt={project.title}
-              style={{
-                top: `${mousePosition.y}px`,
-                left: `${mousePosition.x}px`,
-                transform: `translate(-50%, -50%)`,
-                borderRadius: "8px"
-              }}
-            />
-          </div></Link>
+            <div
+              className="project image-hover"
+              key={index}
+              style={{ "--i": index }}
+            >
+              <span className="project-nr">{index + 1}</span>
+              <h2 className="title">
+                {project.title}
+              </h2>
+
+              <img
+                src={project.image}
+                alt={project.title}
+                style={{
+                  top: `${mousePosition.y}px`,
+                  left: `${mousePosition.x}px`,
+                  transform: `translate(-50%, -50%)`,
+                  borderRadius: "8px"
+                }}
+              />
+            </div></Link>
         ))}
       </div>
     </div>
